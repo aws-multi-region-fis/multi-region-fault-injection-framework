6df397673a91e3927ee685008873fc58621a087d
manaskumar2006 <manaskumar2006@gmail.com>
2025-12-14 21:05:28 -0500

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "primary_lambda_function_name" {
+  value = module.lambda_primary.function_name
+}
+
+output "secondary_lambda_function_name" {
+  value = module.lambda_secondary.function_name
+}
